a5f4814c994d7ebcb3f7c8d21ace29cdf8579bf6
Riicckyy <marcoshenriquerp@hotmail.com>
2022-10-18 16:31:56 -0300

    Studying

---
+++ Flash Chat iOS13/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Henrique Rossi on 23/08/22.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String //show email
+    let body: String
+}
